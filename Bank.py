5b11cae13e88c93d4f8475d18b11da68a4672765
Amitt-44 <workspaceamit44@gmail.com>
2024-10-11 17:24:05 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import sklearn
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
