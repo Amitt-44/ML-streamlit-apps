5b7716422add0c82f499b882d398e93ea4d51f53
Amitt-44 <workspaceamit44@gmail.com>
2024-10-21 01:37:49 +0530

    Update app.py

--- MovieRecommenderSystemApp/app.py
+++ MovieRecommenderSystemApp/app.py
@@ -5,20 +5,19 @@ import requests
 import os
 
 def fetch_poster(movie_id):
-        response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
-        data = response.json()
-        return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
     
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    
     recommended_movies = []
     recommended_movies_posters = []
+        
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        
         recommended_movies.append(movies.iloc[i[0]].title)
         #fetch poster from API
         recommended_movies_posters.append(fetch_poster(movie_id))
