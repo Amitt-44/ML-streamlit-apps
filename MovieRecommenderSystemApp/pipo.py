c8efc724dc36da364b62b35d097f894be298969a
Amitt-44 <workspaceamit44@gmail.com>
2024-10-20 14:51:52 +0530

    Update pipo.py

--- MovieRecommenderSystemApp/pipo.py
+++ MovieRecommenderSystemApp/pipo.py
@@ -2,5 +2,8 @@ import streamlit as st
 import platform
 
 # Display OS information
+
+# Display the Streamlit version
+st.write("Streamlit version:", st.__version__)
 os_info = platform.platform()
 st.write("Operating System:", os_info)
