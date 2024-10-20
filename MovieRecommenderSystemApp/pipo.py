0b911466b0b4afb6fd2dfde9b5b47035e28ca8d7
Amitt-44 <workspaceamit44@gmail.com>
2024-10-20 14:57:45 +0530

    Update pipo.py

--- MovieRecommenderSystemApp/pipo.py
+++ MovieRecommenderSystemApp/pipo.py
@@ -2,6 +2,11 @@ import streamlit as st
 import platform
 import sys
 
+
+# Get Linux distribution information
+distribution = platform.linux_distribution()
+st.write(f"Linux Distribution: {distribution}")
+
 # Display the Python version
 st.write("Python version:", sys.version)
 # Display OS information
